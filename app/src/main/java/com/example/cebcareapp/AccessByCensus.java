349f4223e0c4c0ca41c60561cd87c4ffc0658573
sahan <sahan.sandaruwanwellala@pearson.com>
2019-08-31 23:13:36 +0530

    Made changes in the Bill Payment

---
+++ app/src/main/java/com/example/cebcareapp/AccessByCensus.java
@@ -0,0 +1,14 @@
+package com.example.cebcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccessByCensus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access_by_census);
+    }
+}
--- app/src/main/java/com/example/cebcareapp/PaymentDetails.java
+++ app/src/main/java/com/example/cebcareapp/PaymentDetails.java
@@ -1,8 +1,11 @@
 package com.example.cebcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class PaymentDetails extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class PaymentDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
+
+        Toolbar toolbar = findViewById(R.id.billPaymentToolbar);
+        toolbar.setTitle("Payment Details");
+        setSupportActionBar(toolbar);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String name = bundle.getString("name");
+        String email = bundle.getString("email");
+        String amount = bundle.getString("amount");
+
+        
+
     }
 }
