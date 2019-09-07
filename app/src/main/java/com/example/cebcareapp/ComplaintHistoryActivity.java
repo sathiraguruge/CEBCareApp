ee775f8310b2347d352dac3122022acbd6706e89
sathiraguruge <33770377+sathiraguruge@users.noreply.github.com>
2019-09-07 23:18:52 +0530

    DB Connection added to Complaints

---
+++ app/src/main/java/com/example/cebcareapp/ComplaintHistoryActivity.java
@@ -0,0 +1,24 @@
+package com.example.cebcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import com.example.cebcareapp.Adapter.ComplaintAdapter;
+import com.example.cebcareapp.Database.ComplaintDB;
+
+public class ComplaintHistoryActivity extends AppCompatActivity {
+
+    private ListView listViewComplaints;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_history);
+
+        final ComplaintDB complaintDB = new ComplaintDB(this);
+        this.listViewComplaints = (ListView) findViewById(R.id.listViewComplaints);
+        this.listViewComplaints.setAdapter(new ComplaintAdapter(this, complaintDB.findAll()));
+    }
+}
